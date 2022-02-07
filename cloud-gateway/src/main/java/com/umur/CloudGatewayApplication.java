6d6e1ccec9b4795a00b80ad8155bddc43ec2d693
umur inan <umurinan@gmail.com>
2022-02-07 13:36:59 -0600

    gateway created

--- cloud-gateway/src/main/java/com/umur/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/umur/CloudGatewayApplication.java
@@ -2,12 +2,12 @@ package com.umur;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigServerApplication {
+@EnableEurekaClient
+public class CloudGatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class,args);
+        SpringApplication.run(CloudGatewayApplication.class,args);
     }
 }
