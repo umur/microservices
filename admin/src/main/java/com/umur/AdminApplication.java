2fe8c46f4cb30ea0f8059c270ceb5534fa8e6bcb
umur inan <umurinan@gmail.com>
2022-02-08 14:41:45 -0600

    admin discovery client

--- admin/src/main/java/com/umur/AdminApplication.java
+++ admin/src/main/java/com/umur/AdminApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableAdminServer
+
 public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
