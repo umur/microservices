82d393c71ec6c1879b9d736aac7cf464c8183fdc
umur inan <umurinan@gmail.com>
2022-02-10 20:02:55 -0600

    user service name changed

--- admin/src/main/java/com/umur/AdminApplication.java
+++ admin/src/main/java/com/umur/AdminApplication.java
@@ -2,14 +2,21 @@ package com.umur;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableAdminServer
-
+@EnableDiscoveryClient
 public class AdminApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AdminApplication.class, args);
+       // SpringApplication.run(AdminApplication.class, args);
+        new SpringApplicationBuilder(AdminApplication.class)
+                .web(WebApplicationType.REACTIVE)
+                .run(args);
+
     }
 
 
