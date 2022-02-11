fe877f5c8a6ae232a6e48c391d3f97d9c5e487a3
umur inan <umurinan@gmail.com>
2022-02-10 20:21:30 -0600

    cloud gateway auto discovery with euroka

--- cloud-gateway/src/main/java/com/umur/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/umur/CloudGatewayApplication.java
@@ -2,12 +2,26 @@ package com.umur;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class CloudGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(CloudGatewayApplication.class,args);
     }
+
+    @Bean
+    public DiscoveryClientRouteDefinitionLocator discoveryClientRouteLocator(ReactiveDiscoveryClient dc, DiscoveryLocatorProperties dlp) {
+        dlp.setLowerCaseServiceId(true);
+        return new DiscoveryClientRouteDefinitionLocator(dc,dlp);
+    }
+
 }
