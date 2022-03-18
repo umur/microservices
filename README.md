9069a10efde9c1023beb8089bde681da187c2114
umur inan <umurinan@gmail.com>
2022-03-18 17:42:54 -0500

    SBA

--- README.md
+++ README.md
@@ -8,9 +8,13 @@ Spring Cloud Config Server
 
 ### Spring Cloud API Gateway
 Asynchronous API Gateway
+Service Discovery with Eureka
 
 ### Spring Actuator
 CircuitBreaker, Hysterix, Micrometers, Prometheus 
 
 ### Zipkin - Sleuth
 Distributed Log Tracing
+
+### Spring Boot Admin
+Monitor Microservices
--- service-discovery/src/main/java/com/umur/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/com/umur/ServiceDiscoveryApplication.java
@@ -4,6 +4,8 @@ package com.umur;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -11,4 +13,6 @@ public class ServiceDiscoveryApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceDiscoveryApplication.class,args);
     }
+
+
 }
