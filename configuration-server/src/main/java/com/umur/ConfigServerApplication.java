c62a28765b8961ef9ec20f9cffa2bcf4895c2b80
Umur Inan <umurinan@gmail.com>
2022-04-02 23:37:10 -0500

    config server docker file added

--- configuration-server/src/main/java/com/umur/ConfigServerApplication.java
+++ configuration-server/src/main/java/com/umur/ConfigServerApplication.java
@@ -3,11 +3,24 @@ package com.umur;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class,args);
+    }
+
+    @RequestMapping("/test")
+    @RestController
+    static class TestController{
+
+        @GetMapping
+        public String test(){
+            return  "Hello From Config Server";
+        }
     }
 }
