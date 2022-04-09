67881bc4f8527c9531dff69128173a45d713386e
Umur Inan <umurinan@gmail.com>
2022-04-09 11:09:51 -0500

    user service swagger doc

--- user/src/main/java/com/umur/controller/UserController.java
+++ user/src/main/java/com/umur/controller/UserController.java
@@ -1,6 +1,11 @@
 package com.umur.controller;
 
 import com.umur.domain.Users;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +22,15 @@ public class UserController {
         this.userService=userService;
     }
 
+    @Operation(summary = "Retrieves all users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the user",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Users.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = @Content) })
     @GetMapping
     public List<Users> findAll(){
         return userService.findAll();
